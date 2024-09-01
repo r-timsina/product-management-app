ca5f90e19a6404d9d63bfd2298a43d7f56f440cf
Roshan Timsina <info.roshantimsina@gmail.com>
2024-09-01 11:51:36 +0545

    feat: add unplugin-vue-router

---
+++ src/pages/projects/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    Your Projects Here!
+    <RouterLink :to="{ path: '/' }">Home</RouterLink>
+    <div class="">
+      go to project 1
+      <RouterLink :to="{ name: '/projects/[id]', params: { id: '1' } }">Click Here</RouterLink>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-// import NotFoundView from '@/views/NotFoundView.vue'
-import { h } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/ProjectsView.vue')
-    },
-    {
-      path: '/projects/:id',
-      name: 'single-project',
-      component: () => import('@/views/SingleProjectView.vue')
-    },
-    {
-      path: '/:catchAll(.*)*',
-      name: 'not-found',
-      component: h('p', { style: { color: 'red' } }, '404 page not foun test from hyperscript')
-    }
-  ]
+  routes
 })
 
 export default router
