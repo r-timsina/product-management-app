77d2efaabd21c37857d247d2dfa9ac50656705ed
Roshan Timsina <info.roshantimsina@gmail.com>
2024-11-17 22:27:45 +0545

    fix: remove console

--- database/seed.js
+++ database/seed.js
@@ -3,5 +3,3 @@ import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
-
-console.log(supabase)
