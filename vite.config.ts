8603464c6d698c22aecdea01171a6d9a43fa6703
Roshan Timsina <info.roshantimsina@gmail.com>
2024-11-30 21:54:16 +0545

    feat: add shadcn for UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
+import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
   plugins: [VueRouter({}), vue()],
   resolve: {
     alias: {
