68b3f21e487083835638bbb304ad32374ae0d278
Roshan Timsina <info.roshantimsina@gmail.com>
2024-12-10 17:34:31 +0545

    feat: add shadch input, dropdown and avatar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Input } from '../src/components/ui/input'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+</script>
 
 <template>
   <nav class="w-full h-16 border-b bg-muted/40 flex justify-between items-center px-6">
     <form class="w-full max-w-96">
-      <input class="max-w-96 w-full pl-8 bg-background" type="text" placeholder="Search..." />
+      <Input class="max-w-96 w-full pl-8 bg-background" type="text" placeholder="Search..." />
     </form>
-    <div class="bg-white rounded-full h-8 w-8"></div>
+
+    <DropdownMenu>
+      <Avatar
+        ><AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+        <AvatarFallback>CN</AvatarFallback></Avatar
+      >
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   </nav>
   <main>
     <RouterView />
