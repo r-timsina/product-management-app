39f0f5b2577b492bfcff7d5c5b4d4169f40438c1
Roshan Timsina <info.roshantimsina@gmail.com>
2024-12-10 12:13:23 +0545

    feat: add nav

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-</script>
+<script setup lang="ts"></script>
 
 <template>
+  <nav class="w-full h-16 border-b bg-muted/40 flex justify-between items-center px-6">
+    <form class="w-full max-w-96">
+      <input class="max-w-96 w-full pl-8 bg-background" type="text" placeholder="Search..." />
+    </form>
+    <div class="bg-white rounded-full h-8 w-8"></div>
+  </nav>
   <main>
     <RouterView />
   </main>
