f3d0d5f3b8e67cc049edf664a6b08989b143b56f
Roshan Timsina <info.roshantimsina@gmail.com>
2024-12-11 09:49:25 +0545

    chore: install iconify for icon management

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { Input } from '../src/components/ui/input'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import 'iconify-icon'
 
 import {
   DropdownMenu,
@@ -14,15 +15,21 @@ import {
 
 <template>
   <nav class="w-full h-16 border-b bg-muted/40 flex justify-between items-center px-6">
-    <form class="w-full max-w-96">
+    <form class="w-full max-w-96 relative h-fit">
+      <iconify-icon
+        class="absolute top-[50%] translate-y-[-50%] left-3"
+        icon="lucide:search"
+      ></iconify-icon>
       <Input class="max-w-96 w-full pl-8 bg-background" type="text" placeholder="Search..." />
     </form>
 
     <DropdownMenu>
-      <Avatar
-        ><AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
-        <AvatarFallback>CN</AvatarFallback></Avatar
-      >
+      <DropdownMenuTrigger>
+        <Avatar
+          ><AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+          <AvatarFallback>CN</AvatarFallback></Avatar
+        >
+      </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
