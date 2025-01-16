c7027e4ba60e646e14e0c3d8dacbbce25fffa82c
Roshan Timsina <info.roshantimsina@gmail.com>
2025-01-16 12:29:45 +0545

    refactor: separating column definations

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,9 +1,5 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-
-import type { Tables } from '../../../database/types'
-import type { ColumnDef } from '@tanstack/vue-table'
-import { RouterLink } from 'vue-router'
+import { columns } from '@/utils/tableColumns/projectsColumns'
 import { projectsQuery, type Projects } from '@/utils/supaQueries'
 
 usePageStore().pageData.title = 'Projects'
@@ -17,41 +13,6 @@ const getProjects = async () => {
   }
 }
 await getProjects()
-
-const columns: ColumnDef<Projects[0]>[] = [
-  {
-    accessorKey: 'name',
-    header: () => h('div', { class: 'text-left' }, 'Name'),
-    cell: ({ row }) => {
-      return h(
-        RouterLink,
-        {
-          to: `/projects/${row.original.slug}`,
-          class: 'text-left font-medium hover:bg-muted block w-full'
-        },
-        () => row.getValue('name')
-      )
-    }
-  },
-  {
-    accessorKey: 'status',
-    header: () => h('div', { class: 'text-left' }, 'Status'),
-    cell: ({ row }) => {
-      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
-    }
-  },
-  {
-    accessorKey: 'collaborators',
-    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
-    cell: ({ row }) => {
-      return h(
-        'div',
-        { class: 'text-left font-medium' },
-        JSON.stringify(row.getValue('collaborators'))
-      )
-    }
-  }
-]
 </script>
 
 <template>
