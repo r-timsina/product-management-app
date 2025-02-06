8628d1cdbd9bcee0058e916f4dcfcb965cf81e92
Roshan Timsina <info.roshantimsina@gmail.com>
2025-02-06 23:24:01 +0545

    fix: handle uncaught js errors

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,16 @@
 import AppErrorPage from './components/AppError/AppErrorPage.vue'
 import { useErrorStore } from '@/stores/error'
 
-const { activeError } = storeToRefs(useErrorStore())
+const errorStore = useErrorStore()
+
+onErrorCaptured((error) => {
+  errorStore.setError({ error })
+})
 </script>
 
 <template>
   <AuthLayout>
-    <AppErrorPage v-if="activeError" />
+    <AppErrorPage v-if="errorStore.activeError" />
     <RouterView v-slot="{ Component, route }">
       <Suspense v-if="Component" :timeout="0">
         <Component :is="Component" :key="route.name" />
